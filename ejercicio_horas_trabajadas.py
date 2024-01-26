1d6dae4cc1fc9edb29c2781eb7b772bc15c8bd2d
Juanka20-Sandoval00 <jsandovalq@miumg.edu.gt>
2024-01-25 21:24:47 -0600

    Nuevo ejercicio

---
+++ ejercicio_horas_trabajadas.py
@@ -0,0 +1,6 @@
+horas_trabajadas = float(input("Introduzca el numero de horas trabajadas: "))
+coste_por_hora = float(input("Ingrese el coste que recibe por hora trabajada: "))
+
+dinero_recibido = horas_trabajadas * coste_por_hora
+dinero_recibido = str(dinero_recibido)
+print("El total de dinero recibido por sus horas laboradas es de: " + dinero_recibido)
